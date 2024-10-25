3dd5f653c4afd2ea2b5252abd68fcd9147bf45e2
Guanbao Liang <2031808280@qq.com>
2024-10-25 16:03:41 +0000

    update dec

--- run.py
+++ run.py
@@ -3,12 +3,16 @@ from Incept.utils.data import CommonDataset
 from Incept.utils import seed_everything
 from Incept.exp import ExpManager
 
+# step 0, for testing purpose to set the seed
+seed_everything(42)
+
 # step 1, load config
 config = Incept.configs.load_exp_config("DEC", "MNIST")
-seed_everything(42)
+
 # step 2, load trainer
-# trainer = Incept.models.DECTrainer(config, pretrained = True)
 trainer = Incept.models.DECPretrainer(config)
+# trainer = Incept.models.DECTrainer(config, pretrained = True)
+
 # step 3, load dataset
 train_dataset = CommonDataset(
     config.dataset_name, config.data_dir, True,
@@ -18,7 +22,7 @@ val_dataset = CommonDataset(
     config.dataset_name, config.data_dir, False,
     trainer.img_transform, trainer.target_transform,
 )
-# trainer.train(train_dataset, val_dataset)
+
 # step 4, run experiment
 exp = ExpManager(config, trainer, train_dataset, val_dataset)
 exp.run()
