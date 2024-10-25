c40d1c9920e70883f42a9ff7b003d46c3a962289
Guanbao Liang <2031808280@qq.com>
2024-10-25 06:03:14 +0000

    update dec

--- Incept/exp.py
+++ Incept/exp.py
@@ -1,3 +1,12 @@
+from Incept.utils import seed_everything
+
 class ExpManager:
     def __init__(self, config):
         self.config = config
+    
+    def generate_dataset(self):
+        pass
+    
+    def run(self):
+        seed_everything()
+        
--- Incept/models/__init__.py
+++ Incept/models/__init__.py
@@ -1,2 +1,2 @@
-from DEC.pretrain import DECPretrainer
-from DEC.main import DECTrainer
+from .DEC.pretrain import DECPretrainer
+from .DEC.main import DECTrainer
